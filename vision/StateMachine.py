3c59e4a30497fe1d73dc064429aebae1304b91c1
KimAutoengineer <kimmoellegaard@hotmail.com>
2025-12-01 15:02:13 +0100

    tilføjet statemachine, tilføjet vision funktioner i visionSystem, tilføjet programflow/test envr i visionSystemState

---
+++ vision/StateMachine.py
@@ -0,0 +1,59 @@
+class State:
+    pass
+
+class StateMachine:
+
+    def __init__(self, state:State):
+        self.state = state
+        self.state.stateMachine = self
+
+    def changeState(self, newstate:State):
+        self.state = self.state.Exit()
+        self.state = newstate
+
+        self.state.stateMachine = self
+        self.state.Enter()
+        
+    def run(self):
+        self.state.Enter()
+        self.running = True
+        while self.running:
+            self.state = self.state.Run()
+
+class State:
+    stateMess = None
+    def Enter(self):
+        self.stateMess = "Entering"
+        print(f"Entering state: {self.__class__.__name__}")
+        
+    def Run(self):
+        self.stateMess = "Running"
+        print(f"Running state: {self.__class__.__name__}")
+        
+    def Exit(self):
+        self.stateMess = "Exiting"
+        print(f"Exiting state: {self.__class__.__name__}")
+    
+    def getState(self):
+        return self.stateMess
+
+class Machine(StateMachine):
+    def __init__(self, name,state:State):
+        super().__init__(state)
+        self.name = name
+#----------------------------------------------
+
+
+
+class visionSystem(StateMachine.Machine):
+
+    def __init__(self, state:StateMachine.State):
+
+        super().__init__("visionSystem", state)
+
+        self.targetPos = []        # target position
+        self.currentPos = []       # current position
+        self.objectsDetected = []  # Liste med fundne objekter
+        self.HomePos = []          # home position
+
+    
